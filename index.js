6f1a330f02a598b00913e2e3f874722e600601dd
Ryan Davidson <rdavidson@blizzard.com>
2020-06-11 22:12:28 -0700

    getting the blink on logo to work.

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const documentElement = document.querySelector("html");
 const contentElement = document.querySelector(".main-content");
 const sidebarElement = document.querySelector(".sidebar");
-const sidebarIsOpen = () => documentElement.classList.contains("sidebar-is-open");
+const sidebarIsOpen = () =>
+  documentElement.classList.contains("sidebar-is-open");
 
 const openSidebar = () => {
-    /* How you trigger the change is up to you */
-    documentElement.classList.add("sidebar-is-open");
+  /* How you trigger the change is up to you */
+  documentElement.classList.add("sidebar-is-open");
 };
 
 const closeSidebar = () => {
-    documentElement.classList.remove("sidebar-is-open");
+  documentElement.classList.remove("sidebar-is-open");
 };
 
 const toggleSidebar = () => {
-    sidebarIsOpen() ? closeSidebar() : openSidebar();
+  sidebarIsOpen() ? closeSidebar() : openSidebar();
 };
 console.log("your index.js file is loaded correctly!");
 
-$("#bounce").click(function() {
-    //for (var i = 1; i <= 3; i++) {
-    console.log("hi ryan");
-        $(this).animate({ top: 30 }, "slow");
-        $(this).animate({ top: 0 }, "slow");
-   // }
+$("#bounce").click(function () {
+  
+  if ($(this).hasClass("blink")) {
+    $(this).animate({ opacity: 0.1 }, "bounce");
+    $(this).removeClass("blink");
+  } else {
+    $(this).animate({ opacity: 1 }, "bounce");
+    $(this).addClass("blink");
+  }
 });
 
 function toggleClass() {
-    var element = document.getElementById("contact");
-    element.classList.toggle("active");
+  var element = document.getElementById("contact");
+  element.classList.toggle("active");
 }
